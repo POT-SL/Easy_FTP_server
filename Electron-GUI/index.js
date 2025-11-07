e8786c81414d07b9e26fd985d11dd5ef8b2c7720
nekofox POT <1352220638@qq.com>
2025-11-07 15:56:37 +0800

    2.0 update

---
+++ Electron-GUI/index.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false,
+  })
+
+  win.loadFile('index.html')
+
+  win.setMenu(null);
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
